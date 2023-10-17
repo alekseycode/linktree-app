4012e8c2d104d6036a08ed819abb0561a0f632c7
aleksey <aleksey99@live.com>
2023-10-17 16:28:17 -0700

    refactor backgrounds component

---
+++ src/views/appearance/backgrounds/BackgroundItem.jsx
@@ -0,0 +1,20 @@
+import { Flex, Text, background } from "@chakra-ui/react";
+
+const BackgroundItem = ({ option, border, background }) => {
+  return (
+    <Flex flexDir="column" alignItems="center">
+      <Flex
+        h="200px"
+        w="128px"
+        mb="10px"
+        border={border}
+        borderRadius="10px"
+        cursor="pointer"
+        bg={background}
+      ></Flex>
+      <Text>{option}</Text>
+    </Flex>
+  );
+};
+
+export default BackgroundItem;
---
+++ src/views/appearance/backgrounds/Backgrounds.jsx
@@ -0,0 +1,32 @@
+import { Flex } from "@chakra-ui/react";
+import BackgroundItem from "./BackgroundItem";
+import ColorPicker from "../buttons/items/ColorPicker";
+
+const Backgrounds = ({ styles, updateStyle }) => {
+  return (
+    <Flex flexDir="column" bg="gray.700" borderRadius="30px">
+      <Flex my="28px" mx="16px" justifyContent="space-evenly">
+        <BackgroundItem option="Flat Color" background={styles.bgColor} />
+        <BackgroundItem option="Gradient" border="solid" />
+        <BackgroundItem option="Image" border="dotted" />
+        <BackgroundItem option="Video" border="dotted" />
+      </Flex>
+      <Flex flexDir="column" ml="5%">
+        <ColorPicker
+          heading="Color"
+          background={styles.bgColor}
+          onChange={(color) => updateStyle("bgColor", color.hex)}
+          onKeyDown={(e) => {
+            const val = e.target.value;
+            if (e.key === "Enter" && val) {
+              updateStyle("bgColor", val !== "#" ? val : styles.bgColor);
+            }
+          }}
+          inputVal={styles.bgColor}
+        />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Backgrounds;
