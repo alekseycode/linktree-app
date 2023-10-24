bfcb588a9206c4f0d73f52474029b2c1c13718d7
aleksey <aleksey99@live.com>
2023-10-24 17:59:10 -0400

    style analytics and settings pages

--- src/views/analytics/Analytics.jsx
+++ src/views/analytics/Analytics.jsx
@@ -1,5 +1,13 @@
+import { Flex, Text } from "@chakra-ui/react";
+
 const Analytics = () => {
-  return <div>Analytics</div>;
+  return (
+    <Flex mt="10%">
+      <Text flexGrow="1" textAlign="center" fontSize="2rem">
+        Coming soon
+      </Text>
+    </Flex>
+  );
 };
 
 export default Analytics;
--- src/views/settings/Settings.jsx
+++ src/views/settings/Settings.jsx
@@ -1,5 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <Flex mt="10%">
+      <Text fontSize="2rem">Coming soon</Text>
+    </Flex>
+  );
 };
 
 export default Settings;
