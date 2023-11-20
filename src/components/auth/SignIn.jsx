9b23d511deba5fb3ff8cc0ace544631a291fc2cf
aleksey <aleksey99@live.com>
2023-11-19 21:54:17 -0500

    center sign in page

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -2,7 +2,11 @@ import { Flex } from "@chakra-ui/react";
 import React from "react";
 
 const SignIn = () => {
-  return <Flex mt="10%">Sign In</Flex>;
+  return (
+    <Flex mt="10%" justifyContent="center">
+      Sign In
+    </Flex>
+  );
 };
 
 export default SignIn;
