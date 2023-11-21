b91eaa076f918e5c70c81d841f02ec29532effed
aleksey <aleksey99@live.com>
2023-11-20 23:55:47 -0500

    style sign in page

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,10 +1,22 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Input, Text } from "@chakra-ui/react";
 import React from "react";
 
 const SignIn = () => {
   return (
     <Flex mt="10%" justifyContent="center">
-      Sign In
+      <Flex
+        flexDir="column"
+        alignItems="center"
+        bg="gray.700"
+        borderRadius="10px"
+        h="300px"
+        w="300px"
+      >
+        <Text my="20px">Sign In</Text>
+        <Flex>
+          <Input />
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
