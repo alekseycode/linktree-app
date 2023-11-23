5e5a62587e4d54233418f7b61dc840e94f7e706b
aleksey <aleksey99@live.com>
2023-11-23 00:56:55 -0500

    add sign in functionality

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,33 +1,54 @@
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Text,
-} from "@chakra-ui/react";
-import React from "react";
+import { Button, Flex, Input, Link, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { auth } from "../../auth/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const handleFormChange = (prop, val) => {
+    setFormData((prev) => ({ ...prev, [prop]: val }));
+  };
+
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, formData.email, formData.password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
-    <Flex mt="10%" alignItems="center" flexDir="column">
-      <Text my="40px" fontSize="1.5rem">
-        Sign In
-      </Text>
+    <Flex mt="15%" justifyContent="center">
       <Flex
         flexDir="column"
         alignItems="center"
         bg="gray.700"
-        borderRadius="15px"
-        w="20%"
+        borderRadius="10px"
+        w="400px"
+        p="20px"
+        boxShadow="0px 0px 3px"
       >
-        <Flex w="60%" flexDir="column" py="60px">
-          <Text>Email</Text>
-          <Input type="email" mb="10px" />
-          <Text>Password</Text>
-          <Input type="password" />
-          <Button mt="40px">Submit</Button>
+        <Flex flexDir="column" w="full">
+          <Text mb="5px">Email</Text>
+          <Input
+            name="emailInput"
+            type="email"
+            mb="10px"
+            onChange={(e) => handleFormChange("email", e.target.value)}
+          />
+          <Text mb="5px">Password</Text>
+          <Input
+            name="passwordInput"
+            type="password"
+            onChange={(e) => handleFormChange("password", e.target.value)}
+          />
+          <Flex justifyContent="space-evenly" alignItems="center" mt="30px">
+            <Link color="lightblue">Forgot Password</Link>
+            <Link color="lightblue">Register</Link>
+            <Button color="lightblue" w="80px" onClick={() => signIn()}>
+              Sign In
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
