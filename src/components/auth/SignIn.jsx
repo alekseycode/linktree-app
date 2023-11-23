9375fefbf7cd2a6ea95e99df053171e0269bc934
aleksey <aleksey99@live.com>
2023-11-22 23:58:11 -0500

    refactor sign in page

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,21 +1,33 @@
-import { Flex, Input, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
 const SignIn = () => {
   return (
-    <Flex mt="10%" justifyContent="center">
+    <Flex mt="10%" alignItems="center" flexDir="column">
+      <Text my="40px" fontSize="1.5rem">
+        Sign In
+      </Text>
       <Flex
         flexDir="column"
         alignItems="center"
         bg="gray.700"
         borderRadius="15px"
-        h="300px"
-        w="300px"
+        w="20%"
       >
-        <Text my="20px">Sign In</Text>
-        <Flex flexDir="column">
-          <Input />
-          <Input />
+        <Flex w="60%" flexDir="column" py="60px">
+          <Text>Email</Text>
+          <Input type="email" mb="10px" />
+          <Text>Password</Text>
+          <Input type="password" />
+          <Button mt="40px">Submit</Button>
         </Flex>
       </Flex>
     </Flex>
